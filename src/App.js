7211507652040b5e44a8f58cf72f6968b84d77e3
Nathan Johnson <johnsna7@oregonstate.edu>
2021-08-11 22:25:00 -0500

    add filter object, map and list

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+const FILTER_MAP = {
+	All: () => true,
+	Active: task => !task.completed,
+	Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
 
 	const [tasks, setTasks] = useState(props.tasks);
+	const [filter, setFilter] = useState('All');
 
 	function addTask(name) {
 		const newTask = {
@@ -55,6 +64,10 @@ function App(props) {
 		/>
 	));
 
+	const filterList = FILTER_NAMES.map(name => (
+		<FilterButton key={name} name={name}/>
+	));
+
 	const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
 	const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
@@ -63,9 +76,7 @@ function App(props) {
 			<h1>TodoMatic</h1>
 				<Form onSubmit={addTask} />
 			<div className="filters btn-group stack-exception">
-				<FilterButton />
-				<FilterButton />
-				<FilterButton />
+				{filterList}
 			</div>
 			<h2 id="list-heading">
 				{headingText}
