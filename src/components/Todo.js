a67aa05257139d36e48047e7665613604909e36e
Nathan Johnson <johnsna7@oregonstate.edu>
2021-08-11 19:13:13 -0500

    todo state conditional rendering

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -23,7 +23,7 @@ export default function Todo(props) {
       </div>
     </form>
   );
-  
+
   const viewTemplate = (
     <div className="stack-small">
       <div className="c-cb">
@@ -53,30 +53,8 @@ export default function Todo(props) {
   );
 
   return (
-      <li className="todo stack-small">
-      <div className="c-cb">
-        <input 
-          id={props.id} 
-          type="checkbox" 
-          defaultChecked={props.completed}
-          onChange={() => props.toggleTaskCompleted(props.id)}
-        />
-        <label className="todo-label" htmlFor={props.id}>
-          {props.name}
-        </label>
-      </div>
-      <div className="btn-group">
-        <button type="button" className="btn">
-          Edit <span className="visually-hidden">{props.name}</span>
-        </button>
-        <button 
-          type="button" 
-          className="btn btn__danger"
-          onClick={() => props.deleteTask(props.id)}
-        >
-          Delete <span className="visually-hidden">{props.name}</span>
-        </button>
-      </div>
+    <li className="todo">
+      {isEditing ? editingTemplate : viewTemplate}
     </li>
   );
 }
